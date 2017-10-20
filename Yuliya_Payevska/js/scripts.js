d954c2caf8ec2bedbf1191c4f6466bbf918ede02
Yuliya Payevska <petrikjul@gmail.com>
2017-10-20 22:59:04 +0300

    add calculator

---
+++ Yuliya_Payevska/js/scripts.js
@@ -0,0 +1,61 @@
+/**
+ * Created by John Doe on 17.10.2017.
+ */
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') {
+    initCalculator();
+  }
+};
+
+function initCalculator() {
+  var
+    calculator = document.getElementById('calculator'),
+    first = document.getElementById('first-number'),
+    second = document.getElementById('second-number'),
+    action = document.getElementById('action');
+
+  calculator.onsubmit = function(event) {
+    var
+      firstNumber = parseFloat(first.value),
+      secondNumber = parseFloat(second.value),
+      actionType = action.value.trim(),
+      result = calculate(firstNumber, secondNumber, actionType);
+
+    event.preventDefault();
+
+    resultRender(firstNumber, secondNumber, actionType, result);
+    this.reset();
+  }
+}
+
+function calculate(firstNumber, secondNumber, action) {
+  var result;
+
+  if (action === '+') {
+    result = firstNumber + secondNumber;
+  } else if (action === '-') {
+    result = firstNumber - secondNumber;
+  } else if (action === '*') {
+    result = firstNumber * secondNumber;
+  } else if (action === '/') {
+    result = firstNumber / secondNumber;
+  }
+
+  return result;
+}
+
+function resultRender(firstNumber, secondNumber, action, result) {
+  var
+    results = document.getElementById('results'),
+    resultPreviousLine = results.firstChild,
+    resultLine = document.createElement('li'),
+    resultText = document.createTextNode(firstNumber + ' ' + action + ' ' + secondNumber + ' = ' + result);
+
+  resultLine.appendChild(resultText);
+
+  if (resultPreviousLine) {
+    results.insertBefore(resultLine, resultPreviousLine);
+  } else {
+    results.appendChild(resultLine);
+  }
+}
